425aeef66d77a5aab28dd0de39e015b3ba50d93e
lzx <962745314@qq.com>
2020-08-30 15:47:59 +0800

    死信队列 2020年8月30日15:47:51 add by lzx

--- src/main/java/com/rabbitmq/controller/SendMsgController.java
+++ src/main/java/com/rabbitmq/controller/SendMsgController.java
@@ -25,6 +25,24 @@ public class SendMsgController {
         rabbitTemplate.convertAndSend("my_tt1_queue",msg);
     }
 
+    /**
+     * 过期队列消息投递到死信队列
+     * 投递到正常的队列，但是该队列有过期时间限制，到了过期时间后，消息会被发送到死信交换机（队列）
+     */
+    @RequestMapping("/testTT2")
+    public void tt2QueueTest(@RequestParam String msg) {
+        rabbitTemplate.convertAndSend("my_normal_exchange","my_ttl_dlx",msg);
+    }
+
+    /**
+     * 消息长度超过2会投递到死信队列
+     * 投递到正常的队列，长度超过2以后，消息会被发送到死信交换机（队列）
+     */
+    @RequestMapping("/testTT3")
+    public void tt3QueueTest(@RequestParam String msg) {
+        rabbitTemplate.convertAndSend("my_normal_exchange","my_max_dlx",msg);
+    }
+
     @GetMapping("/sendMsg")
     public String sendMsg(@RequestParam String msg,@RequestParam String key){
         /**
